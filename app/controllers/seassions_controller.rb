09b7a50ae353582ea524b80d3986befdf3c144bf
wadv1232 <wadv1232@yahoo.cn>
2014-03-19 17:07:21 +0800

    Finish sign in

---
+++ app/controllers/seassions_controller.rb
@@ -0,0 +1,21 @@
+class SeassionsController < ApplicationController
+ def new
+ end
+ def create
+ 	 user = User.find_by(email: params[:seassion][:email].downcase)
+ 	 if user && user.authenticate(params[:seassion][:password])
+    # Sign the user in and redirect to the user's show page.
+    sign_in user
+    redirect_to user
+     else
+    # Create an error message and re-render the signin form.
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+     end
+
+ end
+ def destroy
+ 	sign_out
+ 	redirect_to root_path
+ end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,13 @@ describe User do
   
   it {should respond_to(:name)}
   it {should respond_to(:email)}
-it {should respond_to(:password_digest)}
-it {should respond_to(:password)}
-it {should respond_to(:password_confirmation)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
+   
+  it {should respond_to(:remember_token)}
+  it {should respond_to(:authenticate)}
+
   it {should be_valid}
 
   describe "When name is not present" do
@@ -52,42 +56,47 @@ it {should respond_to(:password_confirmation)}
   describe "when email address is already taken" do
     before do
         user_with_same_email=@user.dup
-	user_with_same_email.email=@user.email.upcase
-	user_with_same_email.save
-   end
+	       user_with_same_email.email=@user.email.upcase
+	       user_with_same_email.save
+    end
     it {should_not be_valid}
-end
-
-describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: " ", password_confirmation: " ")
   end
-  it { should_not be_valid }
-end
 
-describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
-
-describe "with a password that's too short" do
-  before { @user.password = @user.password_confirmation = "a"*5}
-  it { should be_invalid}
-end
-describe "return value of authenticate method" do 
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email)}
-  
-  describe "with valid password" do
-   it { should eq found_user.authenticate(@user.password)}
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
   end
-  
-  describe "with invalid password" do
-  let(:user_for_invalid_password) { found_user.authenticate(@user.password)}
 
-  it { should_not eq user_for_invalid_password}
-  specify {expect(user_for_invalid_password).to be_false}
-end
-end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a"*5}
+    it { should be_invalid}
+  end
+  describe "return value of authenticate method" do 
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email)}
+    
+    describe "with valid password" do
+     it { should eq found_user.authenticate(@user.password)}
+    end
+    
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify {user_for_invalid_password should  be_false }
+    end
+  end
+
+  describe "remember token" do
+    before {@user.save}
+    it { expect(@user.remember_token).not_to be_blank }
+  end
 end
